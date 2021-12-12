7480586aeef8bd9cc2821f5806563458956adebe
Lemonawa <69106050+Lemonawa@users.noreply.github.com>
2021-12-12 18:23:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ const readme = `\
 ---
 ⏰ Updated on ${new Date().toUTCString()}
 ---
+![](https://github-readme-stats.vercel.app/api?username=Lemonawa&bg_color=30,e96443,904e95&title_color=fff&text_color=fff&layout=compact)
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=Lemonawa&layout=compact&bg_color=30,e96443,904e95&title_color=fff&text_color=fff)  
+![](https://raw.githubusercontent.com/Lemonawa/Lemonawa/main/assets/github-contribution-grid-snake.svg)
+
+
+<!---
+Lemonawa/Lemonawa is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+\
+`
+
+console.log(readme)
