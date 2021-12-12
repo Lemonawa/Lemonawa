25dd8dbc61e0947f1c416127ef91227cb04b0763
Lemonawa <69106050+Lemonawa@users.noreply.github.com>
2021-12-12 18:24:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ const readme = `\
 ![](https://raw.githubusercontent.com/Lemonawa/Lemonawa/main/assets/github-contribution-grid-snake.svg)
 
 
-<!---
-Lemonawa/Lemonawa is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+
 \
 `
 
