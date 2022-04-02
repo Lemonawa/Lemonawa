cb9e190c7f24e494e37dcacbf3cf22cf07ae8571
Lemonawa <lemonawa1209@gmail.com>
2022-04-03 00:32:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const readme = `\
 ![](https://github-readme-stats.vercel.app/api?username=Lemonawa&bg_color=30,e96443,904e95&title_color=fff&text_color=fff&layout=compact)
 ![](https://github-readme-stats.vercel.app/api/top-langs/?username=Lemonawa&layout=compact&bg_color=30,e96443,904e95&title_color=fff&text_color=fff)  
 ![](https://raw.githubusercontent.com/Lemonawa/Lemonawa/main/assets/github-contribution-grid-snake.svg)
-
-
+# Look How Hard Work The Bot Was
+![Alt](https://repobeats.axiom.co/api/embed/e02bb0793bf59235fcd0d02d38bf634123fc3a78.svg "Repobeats analytics image")
 
 \
 `
