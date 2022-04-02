d4beea71d4a6771432f682cd0cd10bb9b5235c4e
Lemonawa <lemonawa1209@gmail.com>
2022-04-03 00:38:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const readme = `\
 Just a middle school student from China...  
 Learning Python, Html and more.
 ![](https://raw.githubusercontent.com/Lemonawa/Lemonawa/main/assets/github-contribution-grid-snake.svg)
-# ~~Look how hard this robot~~
+###### ~~Look how hard this robot~~
 ![Alt](https://repobeats.axiom.co/api/embed/e02bb0793bf59235fcd0d02d38bf634123fc3a78.svg "Repobeats analytics image")
 
 \
