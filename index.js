42e828f2f47f383ae78137c5e25651c5e2c1ad9b
Lemonawa <lemonawa1209@gmail.com>
2022-04-09 22:10:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generateProgressBar() {
 }
 // README.md Start
 const readme = `\
-### Hi there
+### Hi there 🖐️
 14岁，是学生  
 ~~Also a gay~~  
 在学Html/css/js  
@@ -22,7 +22,6 @@ const readme = `\
 ![](https://raw.githubusercontent.com/Lemonawa/Lemonawa/main/assets/github-contribution-grid-snake.svg)
 ###### ~~Look how hard this robot~~
 ![Alt](https://repobeats.axiom.co/api/embed/e02bb0793bf59235fcd0d02d38bf634123fc3a78.svg "Repobeats analytics image")
-<script async defer data-website-id="3b04f917-8a6d-45fe-989e-9266e14f466b" src="https://umami.lemonawa.xyz/umami.js"></script>
 \
 `
 
